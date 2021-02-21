567009b3e95c877de68dc6f260214bf49ca567b9
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2021-02-21 03:53:00 -0800

    Updated

--- Section_10.h
+++ Section_10.h
@@ -4,24 +4,36 @@ using namespace std;
 
 int Crypto()
 {
-	string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
-	string key{ "XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr" };
-	string orig_phrase, crypted_phrase, decrypted_phrase;
-	
+	string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ " };
+	string key{ "XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr%" };
+	string orig_phrase{}, crypted_phrase{}, decrypted_phrase{};
+	/**/
 	cout << "Enter phrase to crypt: \n";
-	getline (cin, orig_phrase);
-
-	for (size_t i{0}; i < orig_phrase.length(); ++i)
+	getline(cin, orig_phrase);
+	/**/
+	for (size_t i{ 0 }; i < orig_phrase.length(); ++i)
 	{
-		for (size_t j{ 0 }; alphabet.length(); ++j)
+		for (size_t j{ 0 }; j < alphabet.length(); ++j)
 		{
-			if (orig_phrase.at(i) == alphabet.at(j)) 
+			if (orig_phrase.at(i) == alphabet.at(j))
 			{
 				crypted_phrase += key[j];
 			}
 		}
 	}
-	// a;; 
-	cout << "Crypted phrase: \n", crypted_phrase;
+	cout << "Crypted phrase: \n" << crypted_phrase;
+	/**/
+	for (size_t i{ 0 }; i < crypted_phrase.length(); ++i)
+	{
+		for (size_t j{ 0 }; j < key.length(); ++j)
+		{
+			if (crypted_phrase.at(i) == key.at(j))
+			{
+				decrypted_phrase += alphabet[j];
+			}
+		}
+	}
+	cout << "\nDerypted phrase: \n" << decrypted_phrase;
+	/**/
 	return 0;
 }
