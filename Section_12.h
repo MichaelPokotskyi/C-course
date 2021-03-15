e3162c55c441a47a77c26d43d22b1739ee1c0fe5
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2021-03-14 18:45:35 -0700

    up

--- Section_12.h
+++ Section_12.h
@@ -1,19 +1,50 @@
 #pragma once
 #include <iostream>
-#include <vector>
 using namespace std;
 
+void print(int[], size_t);
+int* apply_all(int[], int, int[], int);
+
 void s12() {
+	/*
 	int *arr_ptr{nullptr};
 	int size{};
 	cout << "How big? ";
 	cin >> size;
 	arr_ptr = new int{ size };
-	
+
 	for (int i{0}; i < size; ++i) {
-		arr_ptr[i] = i;
 		cout << &arr_ptr[i] << " " <<  arr_ptr[i] << endl;
 	}
-//	char t{};
-//	cin >> t;
+	*/
+	
+	const size_t array1_size{ 5 };
+	const size_t array2_size{ 3 };
+
+	int arr1[]{ 1,2,3,4,5 };
+	int arr2[]{ 10, 20,30 };
+
+	cout << "Array 1: ";
+	print(arr1, array1_size);
+
+	cout << "Array 2: ";
+	print(arr2, array2_size);
+
+	int *results = apply_all(arr1, array1_size, arr2, array2_size);
+	constexpr size_t res{ array1_size * array2_size };
+
+	cout << "Result: ";
+	print(results, res);
+
+}
+
+void print(int arr[], size_t size) 
+{
+
+}
+
+
+int* apply_all(int arr1[], int size1, int arr2[], int size2) 
+{
+	return 0;
 }
