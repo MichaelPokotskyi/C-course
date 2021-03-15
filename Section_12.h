9e3515d4d589f892a89a28504896cd89f6bf4685
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2021-03-14 19:03:50 -0700

    up

--- Section_12.h
+++ Section_12.h
@@ -21,8 +21,8 @@ void s12() {
 	const size_t array1_size{ 5 };
 	const size_t array2_size{ 3 };
 
-	int arr1[]{ 1,2,3,4,5 };
-	int arr2[]{ 10, 20,30 };
+	int arr1[]{ 1, 2, 3, 4, 5 };
+	int arr2[]{ 10, 20, 30 };
 
 	cout << "Array 1: ";
 	print(arr1, array1_size);
@@ -31,20 +31,33 @@ void s12() {
 	print(arr2, array2_size);
 
 	int *results = apply_all(arr1, array1_size, arr2, array2_size);
-	constexpr size_t res{ array1_size * array2_size };
+	//constexpr size_t res{ array1_size * array2_size };
+	size_t res{ array1_size * array2_size };
 
 	cout << "Result: ";
-	print(results, res);
+	//print(results, res);
 
 }
 
-void print(int arr[], size_t size) 
+void print(int arr[], size_t size)
 {
-
+	cout << "[";
+	for (size_t i{ 0 }; i < size; ++i)
+	{
+		if (i != size - 1)
+		{
+			cout << arr[i] << ", ";
+		}
+		else
+		{
+			cout << arr[i];
+		}
+	}
+	cout << "]";
+	cout << endl;
 }
 
-
-int* apply_all(int arr1[], int size1, int arr2[], int size2) 
+int* apply_all(int arr1[], int size1, int arr2[], int size2)
 {
 	return 0;
 }
