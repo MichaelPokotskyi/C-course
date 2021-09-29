74a385fd9ea7b9bcf6cdf3e33814376ff7f93cc0
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2021-09-29 04:49:43 -0700

    upd

---
+++ Section_14.cpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include "Section_14.h"
+
+using namespace std;
+
+// no args constructor
+Mystring::Mystring()
+	: str{nullptr} {
+	str = new char[1];
+	*str = '\0';
+}
+
+//Overloaded constructor
+Mystring::Mystring(const char* s) 
+	:str{nullptr} {
+	if (s == nullptr) {
+		str = new char[1];
+		*str = '\0';
+	}
+	else {
+		str = new char[strlen(s) + 1];
+		strcpy(str, s);
+	}
+}
+
+//Copy constructor
+Mystring::Mystring(const Mystring &source) 
+	: str{nullptr} {
+	str = new char[strlen(str) + 1];
+	strcpy(str, source.str);
+}
+
+//Destructor
+Mystring::~Mystring() {
+	delete [] str;
+}
+
+//Copy assignment
+Mystring &Mystring::operator=(const Mystring& rhs) {
+	if (this == &rhs) {
+		return *this;
+		delete[] this->str;
+	} 
+	str = new char[strlen(rhs.str) + 1];
+	strcpy(this->str, rhs.str);
+	return *this;
+}
+
+//Getters implementation
+void Mystring::display() const {
+	cout << str << " : " << get_length() << endl;
+}
+
+int Mystring::get_length() const {
+	return strlen(str);
+}
+
+//const char* get_str() const;
--- Section_14.h
+++ Section_14.h
@@ -1,10 +1,14 @@
 #pragma once
+#ifndef _Section_14_H_
+#define _Section_14_H_
+
 using namespace std;
 
 class Mystring
 {
 private:
-	char* str; // pointer to a char[] that holds a C-style string
+	char *str; // pointer to a char[] that holds a C-style string
+
 public:
 	Mystring(); // no-args constructor
 	Mystring(const char* s); // overloaded constructor
@@ -12,6 +16,7 @@ public:
 	~Mystring(); // destructor
 
 	void display() const;
-	int get_length() const;
+	int get_length() const; // getters
 	const char* get_str() const;
 };
+#endif
