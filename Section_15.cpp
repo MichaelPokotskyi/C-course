396f35a6888608618d6a4f52331a901efcd9b8cd
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2022-08-31 07:06:04 -0700

    up

--- Section_15.cpp
+++ Section_15.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "Section_15.h"
 
 // Running by S15() from main ->
 
-void s15() {}
+void s15() {
+    cout.precision(2);
+    cout << fixed;
+    string str;
+
+    // Accounts
+    vector<Account> accounts;
+    accounts.push_back(Account{});
+    accounts.push_back(Account{ "Larry" });
+    accounts.push_back(Account{ "Moe", 2000 });
+    accounts.push_back(Account{ "Curly", 5000 });
+
+    display(accounts);
+    deposit(accounts, 1000);
+    withdraw(accounts, 2000);
+
+    // Savings 
+
+    vector<Savings_Account> sav_accounts;
+    sav_accounts.push_back(Savings_Account{});
+    sav_accounts.push_back(Savings_Account{ "Superman" });
+    sav_accounts.push_back(Savings_Account{ "Batman", 2000 });
+    sav_accounts.push_back(Savings_Account{ "Wonderwoman", 5000, 5.0 });
+
+    display(sav_accounts);
+    deposit(sav_accounts, 1000);
+    withdraw(sav_accounts, 2000);
+
+    cin >> str;
+
+    return 0;
+}
 
 
 //Account.cpp ****
