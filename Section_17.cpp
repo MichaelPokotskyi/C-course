0eda12362a2d8963c2ea5199e572b9f2c9db11c3
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2022-09-23 06:59:54 -0700

    up

--- Section_17.cpp
+++ Section_17.cpp
@@ -1,13 +1,22 @@
 using namespace std;
 #include "Section_17.h"
 
+//func implementation
+unique_ptr<vector<shared_ptr<Test>>> make() {
+
+    unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
+    return vec_ptr;
+}
+void fill(vector<shared_ptr<Test>>& vec, int num) {}
+void display17(const vector<shared_ptr<Test>>& vec) {}
+
 int s17() {
-	// function S17 called from main
+	// function S17 called from main, testing
     unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
     vec_ptr = make();
     cout << "How many data points do you want to enter: ";
     int num;
-    std::cin >> num;
+    cin >> num;
     fill(*vec_ptr, num);
     display17(*vec_ptr);
     return 0;
--- Section_17.h
+++ Section_17.h
@@ -4,13 +4,6 @@ using namespace std;
 #include <memory>
 #include <vector>
 
-
-// func prototypes
-int s17();
-unique_ptr<vector<shared_ptr<Test>>> make();
-void fill(vector<shared_ptr<Test>>& vec, int num);
-void display17(const vector<shared_ptr<Test>>& vec);
-
 //class TEST
 class Test {
 private:
@@ -22,11 +15,8 @@ public:
     ~Test() { cout << "\tTest destructor (" << data << ")" << endl; }
 };
 
-//func implementation
-unique_ptr<vector<shared_ptr<Test>>> make() {
-
-    unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
-    return vec_ptr;
-}
-void fill(vector<shared_ptr<Test>>& vec, int num) {}
-void display17(const vector<shared_ptr<Test>>& vec){}
+// func prototypes
+int s17();
+unique_ptr<vector<shared_ptr<Test>>> make();
+void fill(vector<shared_ptr<Test>>& vec, int num);
+void display17(const vector<shared_ptr<Test>>& vec);
