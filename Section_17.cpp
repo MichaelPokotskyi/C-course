b88f9111572e391aef6fcf0bbff1c888cb7c9946
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2022-09-23 06:40:25 -0700

    up

--- Section_17.cpp
+++ Section_17.cpp
@@ -1,8 +1,15 @@
+using namespace std;
 #include "Section_17.h"
 
 int s17() {
 	// function S17 calledc from main
-
-	return 0;
+    unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
+    vec_ptr = make();
+    std::cout << "How many data points do you want to enter: ";
+    int num;
+    std::cin >> num;
+    fill(*vec_ptr, num);
+    display17(*vec_ptr);
+    return 0;
 }
 
--- Section_17.h
+++ Section_17.h
@@ -1,5 +1,31 @@
 #pragma once
 using namespace std;
+#include <iostream>
+#include <memory>
+#include <vector>
+
 
 // func prototypes
 int s17();
+unique_ptr<vector<shared_ptr<Test>>> make();
+void fill(vector<shared_ptr<Test>>& vec, int num);
+void display17(const vector<shared_ptr<Test>>& vec);
+
+//class TEST
+class Test {
+private:
+    int data;
+public:
+    Test() : data{ 0 } { cout << "\tTest constructor (" << data << ")" << endl; }
+    Test(int data) : data{ data } { cout << "\tTest constructor (" << data << ")" << endl; }
+    int get_data() const { return data; }
+    ~Test() { cout << "\tTest destructor (" << data << ")" << endl; }
+};
+
+//func implementation
+unique_ptr<vector<shared_ptr<Test>>> make() {
+
+    return unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
+}
+void fill(vector<shared_ptr<Test>>& vec, int num) {}
+void display17(const vector<shared_ptr<Test>>& vec){}
