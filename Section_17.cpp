6e77c1a0016810f776fcd2fb805bc8de5b2a4cba
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2022-09-23 06:50:23 -0700

    up

--- Section_17.cpp
+++ Section_17.cpp
@@ -2,10 +2,10 @@ using namespace std;
 #include "Section_17.h"
 
 int s17() {
-	// function S17 calledc from main
+	// function S17 called from main
     unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
     vec_ptr = make();
-    std::cout << "How many data points do you want to enter: ";
+    cout << "How many data points do you want to enter: ";
     int num;
     std::cin >> num;
     fill(*vec_ptr, num);
--- Section_17.h
+++ Section_17.h
@@ -18,14 +18,15 @@ private:
 public:
     Test() : data{ 0 } { cout << "\tTest constructor (" << data << ")" << endl; }
     Test(int data) : data{ data } { cout << "\tTest constructor (" << data << ")" << endl; }
-    int get_data() const { return data; }
+    int get_data() const { return data; } //getter func
     ~Test() { cout << "\tTest destructor (" << data << ")" << endl; }
 };
 
 //func implementation
 unique_ptr<vector<shared_ptr<Test>>> make() {
 
-    return unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
+    unique_ptr<vector<shared_ptr<Test>>> vec_ptr;
+    return vec_ptr;
 }
 void fill(vector<shared_ptr<Test>>& vec, int num) {}
 void display17(const vector<shared_ptr<Test>>& vec){}
